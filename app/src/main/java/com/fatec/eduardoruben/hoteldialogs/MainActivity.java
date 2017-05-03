ff8a2daaedcad0efcb6e38e716a15bdea733569d
Eduardo Pereira Ribeiro <eduardo.ribeiro25@fatec.sp.gov.br>
2017-05-02 21:45:57 -0300

    Inclusão de algumas strings para as ListViews.

--- app/src/main/java/com/fatec/eduardoruben/hoteldialogs/MainActivity.java
+++ app/src/main/java/com/fatec/eduardoruben/hoteldialogs/MainActivity.java
@@ -1,11 +1,9 @@
 package com.fatec.eduardoruben.hoteldialogs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lista = (ListView) findViewById(R.id.listaPrincipal);
 
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.equipes, android.R.layout.simple_list_item_1);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.main, android.R.layout.simple_list_item_1);
         lista.setAdapter(adapter);
 
     }
