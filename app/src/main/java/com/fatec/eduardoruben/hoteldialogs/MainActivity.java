2a44e3374943f9ea7430f5c56a8d34852f1992c6
Eduardo Pereira Ribeiro <eduardo.ribeiro25@fatec.sp.gov.br>
2017-05-03 22:46:28 -0300

    Conclusão das strings para ListViews

--- app/src/main/java/com/fatec/eduardoruben/hoteldialogs/MainActivity.java
+++ app/src/main/java/com/fatec/eduardoruben/hoteldialogs/MainActivity.java
@@ -2,6 +2,8 @@ package com.fatec.eduardoruben.hoteldialogs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.main, android.R.layout.simple_list_item_1);
         lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
 
     }
 
