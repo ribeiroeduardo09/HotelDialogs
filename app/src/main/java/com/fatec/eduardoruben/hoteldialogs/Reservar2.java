7f788a8c0b13e7396904604246a8b63bee1853d1
Eduardo Pereira Ribeiro <eduardo.ribeiro25@fatec.sp.gov.br>
2017-05-10 22:14:36 -0300

    Inclusão da Activity "Reservar"

--- app/src/main/java/com/fatec/eduardoruben/hoteldialogs/Reservar2.java
+++ app/src/main/java/com/fatec/eduardoruben/hoteldialogs/Reservar2.java
@@ -2,24 +2,37 @@ package com.fatec.eduardoruben.hoteldialogs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class Reservar2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reservar2);
+
+        //Floating button começa aqui!
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.speak);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         //ListView começa aqui!!!
 
-        ListView lista = (ListView) findViewById(R.id.listaPrincipal);
+        ListView lista = (ListView) findViewById(R.id.listaReservar);
 
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.main, android.R.layout.simple_list_item_1);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.reservar, android.R.layout.simple_list_item_1);
         lista.setAdapter(adapter);
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -55,9 +68,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
-
-
-    //ListView termina aqui!!!
 }
