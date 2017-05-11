72ce73988f275a2fc55a358e95e86fa2ce050bdf
ribeiroeduardo09 <ribeiroeduardo09@gmail.com>
2017-05-11 20:59:38 -0300

    Inclusão das Activities "Avaliacao", "Checkout", "Geral" e "Pagamento"

---
+++ app/src/main/java/com/fatec/eduardoruben/hoteldialogs/Geral.java
@@ -0,0 +1,72 @@
+package com.fatec.eduardoruben.hoteldialogs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Geral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkin);
+
+        //Floating button começa aqui!
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.speak);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //ListView começa aqui!!!
+
+        ListView lista = (ListView) findViewById(R.id.listaGeral);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.geral, android.R.layout.simple_list_item_1);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), Reservar2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+                if (position == 1) {
+                    Intent myIntent = new Intent(view.getContext(), Reservar2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+                if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), Reservar2.class);
+                    startActivityForResult(myIntent, 0);
+                }/*
+                if (position == 3) {
+                    Intent myIntent = new Intent(view.getContext(), Reservar2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+                if (position == 4) {
+                    Intent myIntent = new Intent(view.getContext(), Reservar2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+                if (position == 5) {
+                    Intent myIntent = new Intent(view.getContext(), Reservar2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+                if (position == 6) {
+                    Intent myIntent = new Intent(view.getContext(), Reservar2.class);
+                    startActivityForResult(myIntent, 0);
+                }*/
+            }
+        });
+    }
+}
