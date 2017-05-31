0a8712b5a9425e1aac5095f463bcbb6b62751bcd
filiperdt <rubenfilipeao@gmail.com>
2017-05-30 22:27:12 -0300

    Criação da classe TextoVoz
    Texto para voz

---
+++ app/src/main/java/com/fatec/eduardoruben/hoteldialogs/TextoVoz.java
@@ -0,0 +1,13 @@
+package com.fatec.eduardoruben.hoteldialogs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextoVoz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_texto_voz);
+    }
+}
